0310f73e9c3c5a4eeaf1ce0d6b535a3d2af71142
caiojulian <caiojulian@hotmail.com>
2022-04-13 10:46:07 -0300

    dialog card component

--- ui.frontend/src/components/Card/Card.vue
+++ ui.frontend/src/components/Card/Card.vue
@@ -1,22 +1,34 @@
 <template>
   <section class="card">
-    <h1 class="card__title">I have bad news for you</h1>
-    <p class="card__description">
-      The page you are looking for might be removed or is temporarily
-      unavailable
-    </p>
+    <h1 class="card__title">{{ title }}</h1>
+    <p class="card__description">{{ description }}</p>
     <button class="card__button">
-      <span class="card__button-title">Back to homepage</span>
+      <span class="card__button-title">{{ buttonTitle }}</span>
     </button>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'Card'
-}
+  name: "Card",
+  props: {
+    title: {
+      type: String,
+      default: "I have bad news for you",
+    },
+    description: {
+      type: String,
+      default:
+        "The page you are looking for might be removed or is temporarily unavailable",
+    },
+    buttonTitle: {
+      type: String,
+      default: "Back to homepage",
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import "./card.scss"
+@import "./card.scss";
 </style>
