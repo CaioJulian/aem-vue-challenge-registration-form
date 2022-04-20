f9428877bb97966ddbe164f5602185c5ffed2b70
caiojulian <caiojulian@hotmail.com>
2022-04-20 14:08:19 -0300

    fix: layout desk headerHome component

--- ui.frontend/src/components/Home/HeaderHome.vue
+++ ui.frontend/src/components/Home/HeaderHome.vue
@@ -3,7 +3,11 @@
     <figure class="header-home__figure">
       <img :src="src" alt="logo compass" width="150" height="43" />
     </figure>
-    <section class="header-home__card">
+    <section class="header-home__card-middle">
+      <p class="header-home__clock">11:26</p>
+      <p class="header-home__date">terça-feira, 17 de março de 2022</p>
+    </section>
+    <section class="header-home__card-right">
       <p class="header-home__location">{{ location }}</p>
       <p class="header-home__temp">{{ temp }}°</p>
     </section>
@@ -15,7 +19,7 @@ export default {
   props: {
     src: {
       type: String,
-      default: '/img/logo-compass.png'
+      default: '/content/dam/vue/img/logo-compass.png'
     }
   },
   data () {
@@ -27,43 +31,6 @@ export default {
 }
 </script>
 
-<style lang="scss">
-.header-home {
-  display: flex;
-  justify-content: space-between;
-  padding: 2rem 1rem;
-  text-align: center;
-  color: #222222;
-
-  &__figure {
-    margin: 0;
-  }
-
-  &__card {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-  }
-
-  &__location {
-    margin: 0;
-    font-weight: 400;
-    font-size: 0.875rem;
-    line-height: 1.125rem;
-  }
-
-  &__temp {
-    margin: 0;
-    font-weight: 700;
-    font-size: 3rem;
-    line-height: 3.813rem;
-
-    &::before {
-      content: url("/img/icons/cloudy.png");
-      position: relative;
-      top: -10px;
-      left: -10px;
-    }
-  }
-}
+<style lang="scss" scoped>
+@import "./headerHome.scss"
 </style>
