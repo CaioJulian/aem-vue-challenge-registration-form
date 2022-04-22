9e5831cc79b9f8e96418566cea1267dea74e2174
caiojulian <caiojulian@hotmail.com>
2022-04-22 18:43:46 -0300

    feat: loginManager store

---
+++ ui.frontend/src/store/loginManager.js
@@ -0,0 +1,35 @@
+import people from '@/server/people.json'
+
+export const state = () => ({
+  userLogin: {},
+  isValid: true
+})
+
+export const mutations = {
+  setLogin (state, payload) {
+    state.userLogin = payload
+  },
+
+  setIsValid (state, payload) {
+    state.isValid = payload
+  }
+}
+
+export const actions = {
+  fetchLogin ({ commit }, form) {
+    const { logins } = people
+    const { user, password } = form
+    let login = {}
+
+    if (logins && logins.length > 1) {
+      login = logins.find((e) => e.login === user && e.password === password)
+    }
+
+    if (!login) {
+      login = {}
+      commit('setIsValid', false)
+    }
+
+    commit('setLogin', login)
+  }
+}
