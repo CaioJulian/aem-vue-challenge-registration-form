c63538ab3edfa15344990c27d56e7131df3b68c1
caiojulian <caiojulian@hotmail.com>
2022-06-01 18:25:58 -0300

    feat: cardFinish component

--- ui.frontend/src/components/Form/CardForm.vue
+++ ui.frontend/src/components/Form/CardForm.vue
@@ -1,31 +1,36 @@
 <template>
   <section class="container">
     <section class="card-form">
-      <article class="card-form__header">
-        <p class="card-form__title">Team Sign Up</p>
-      </article>
-      <NavTabs :navs="navTabs" />
-      <article class="card-form__body">
-        <div
-          class="card-form__pane"
-          v-for="(pane, p) in navTabs"
-          :key="`pane-${p}`"
-          :id="idName(pane.tabName)"
-          :class="{ 'card-form__pane--active': isActive(`${pane.tabName}`) }"
-        >
-          <InputGroup
-            v-for="(item, i) in pane.inputs"
-            :key="`input-${i}`"
-            :item="item"
-          />
-          <button-form
-            v-if="pane.buttonName"
-            :text="pane.buttonName"
-            :isFinish="checkFinish(navTabs.length, p)"
-            :clickButton="clickButton(p)"
-          />
-        </div>
-      </article>
+      <template v-if="isActive('finish')">
+        <CardFinish />
+      </template>
+      <template v-else>
+        <article class="card-form__header">
+          <p class="card-form__title">Team Sign Up</p>
+        </article>
+        <NavTabs :navs="navTabs" />
+        <article class="card-form__body">
+          <div
+            class="card-form__pane"
+            v-for="(pane, p) in navTabs"
+            :key="`pane-${p}`"
+            :id="idName(pane.tabName)"
+            :class="{ 'card-form__pane--active': isActive(`${pane.tabName}`) }"
+          >
+            <InputGroup
+              v-for="(item, i) in pane.inputs"
+              :key="`input-${i}`"
+              :item="item"
+            />
+            <button-form
+              v-if="pane.buttonName"
+              :text="pane.buttonName"
+              :isFinish="checkFinish(navTabs.length, p)"
+              :clickButton="clickButton(p)"
+            />
+          </div>
+        </article>
+      </template>
     </section>
   </section>
 </template>
@@ -35,6 +40,7 @@ import InputGroup from '../micro/InputGroup.vue'
 import NavTabs from '../micro/NavTabs.vue'
 import ButtonForm from '../micro/ButtonForm.vue'
 import { mapState } from 'vuex'
+import CardFinish from '../micro/CardFinish.vue'
 export default {
   props: {
     navTabs: {
@@ -42,7 +48,7 @@ export default {
       default: () => []
     }
   },
-  components: { InputGroup, NavTabs, ButtonForm },
+  components: { InputGroup, NavTabs, ButtonForm, CardFinish },
   computed: {
     ...mapState('formManager', ['activeItem'])
   },
@@ -59,7 +65,7 @@ export default {
     },
     clickButton(index) {
       const isLast = this.checkFinish(this.navTabs.length, index)
-      const tabName = !isLast ? this.navTabs[index + 1].tabName : ''
+      const tabName = !isLast ? this.navTabs[index + 1].tabName : 'finish'
       return tabName
     }
   }
